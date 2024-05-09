f930d6a0883aba666aa76ef92a9a88d4b4a4c73e
vlabs-dev-nitk <140144238+VlabDevNITK@users.noreply.github.com>
2024-05-09 12:16:40 +0530

    Update contents.js

--- assets/js/contents.js
+++ assets/js/contents.js
@@ -1,7 +1,7 @@
 "use strict";
 const domain = "..";
 const lab = "Disaster Management Lab";
-const exp = "Exploring morse code : Numbers";
+const exp = "Exploring morse code  :  Numbers";
 const domainName = document.querySelector(".domainName");
 const labName = document.querySelector(".labName");
 const expName = document.querySelector(".expName");
